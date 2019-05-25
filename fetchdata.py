e0cb0ee526f25797d6b32c307b3b72d1d942af9c
knyak <knyak@knayk>
2019-05-25 10:18:42 +0000

    final push

--- fetchdata.py
+++ fetchdata.py
@@ -28,4 +28,35 @@ for state in state_dict:
             clinks.append(urltemp%(state,cid,cid))
             
 def getdata(url):
+    soup = BeautifulSoup(requests.get(url).content,"html.parser")
+    table = soup.find("table",{"class","table-party"})
+    c_data = {}
+    c_data["constituency"] = table.find("tr").text.strip()
+    c_data["url"] = url
+    headings = []
+    for h in table.find("tr",{"style":"height: 20px; background-color: #FFC0CD; color:Black;"}).find_all("th"):
+        headings.append(h.text.strip())
     
+    trs = table.find_all("tr")[3:]
+    candidates = trs[:-1]
+    candidates_dict = []
+    for candidate in candidates:
+        data = []
+        for d in candidate.find_all("td"):
+            data.append(d.text.strip())
+        candidates_dict.append(dict(zip(headings,data)))
+    c_data["candidates"] = candidates_dict
+    return c_data
+
+results = []
+
+count = 1
+for link in clinks:
+    print("Fetching(%s): %s"%(count,link))
+    count = count+1
+    results.append(getdata(link))
+    
+with open("results.json","w") as res:
+    res.write("[\n")
+    res.write(",\n".join(list(map(json.dumps,results))))
+    res.write("]")
